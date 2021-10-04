df108e5d1afad66aaabfb2a4b12c0cf9e6288e02
Kaleb-Aguirre03 <820930@mystma.org>
2021-10-04 09:37:53 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the function hasL()
+def hasL(w):
+
+#Create a for loop to check every letter in a word
+    for i in range (0, len(w)):
+        if w[i] == "l":
+            return True
+
+    #else statement is not needed because it is not on the same indentation as the if statement        
+    return False
+
+#Print the function to test the code
+print (hasL("wool"))
